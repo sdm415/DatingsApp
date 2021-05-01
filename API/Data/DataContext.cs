dce8d42fffec3367514026a279bf2c05fe97ccb8
sdm415 <saddams50@gmail.com>
2021-05-02 02:59:48 +0530

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Entities;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
